2374a3d4c53b2c34f210f9ecdcda437aad0a80e9
= <alexander.alegre@outlook.com>
2017-08-30 20:06:59 -0700

    final app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,55 @@
 $(document).ready(function() {
   // FIREBASE CONFIG
   var config = {
-
+    apiKey: 'AIzaSyD-r4wUfuLYmzPIzjYBCjFq8cYYlBX3exg',
+    authDomain: 'fir-auth-b8391.firebaseapp.com',
+    databaseURL: 'https://fir-auth-b8391.firebaseio.com',
+    projectId: 'fir-auth-b8391',
+    storageBucket: '',
+    messagingSenderId: '1005599541893'
   };
   // INITIALIZE FIREBASE
-  
+  firebase.initializeApp(config);
   // CHECK CURRENT PATH
-  
+  var currentPath = $(location)[0].pathname;
   // CHECK IF USER IS SIGNED IN
- 
+ firebase.auth().onAuthStateChanged(function(user) {
+  if(user && (currentPath === '/index.html' || currentPage === '/')) {
+    $(location).attr('href', 'auth.html');
+  } else if (!user && currentPath === '/auth.html') {
+    $(location).attr('href', 'index.html');
+  }
+ });
   // SIGN IN THE USER
-
+  $('#si-btn').on('click', function() {
+    var email = $('#si-email').val().trim();
+    var password = $('#si-password').val().trim();
+    if(email && password) {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+        $(location).attr('href', 'auth.html');
+      }).catch(function(error) {
+        alert(error.message);
+      });
+    }
+  });
   // SIGN UP THE USER
-
+  $('#su-btn').on('click', function() {
+    var email = $('#su-email').val();
+    var password = $('#su-password').val();
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+      $(location).attr('href', 'auth.html');
+    }).catch(function(error) {
+      alert(error.message);
+    });
+  });
   // SIGN OUT THE USER
-
+  $('#sign-out').on('click', function() {
+    firebase.auth().signOut().then(function() {
+        $(location).attr('href', 'index.html');
+    }).catch(function(error) {
+      alert(error.message);
+    });
+  });
 
   /*****************
   JAVASCRIPT FOR CSS
